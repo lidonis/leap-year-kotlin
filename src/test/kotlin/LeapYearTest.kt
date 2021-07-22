e3d9f7ec4afb99bc5dca1496ee06bdd41de3f9fe
lidonis <lidonis.calhau@gmail.com>
2021-07-22 23:15:39 +0200

    refactor: use property based testing for leap years

--- src/test/kotlin/LeapYearTest.kt
+++ src/test/kotlin/LeapYearTest.kt
@@ -1,4 +1,3 @@
-import io.kotest.common.ExperimentalKotest
 import io.kotest.core.spec.style.ExpectSpec
 import io.kotest.datatest.withData
 import io.kotest.matchers.shouldBe
@@ -7,7 +6,6 @@ import io.kotest.property.arbitrary.filter
 import io.kotest.property.arbitrary.int
 import io.kotest.property.checkAll
 
-@ExperimentalKotest
 class LeapYearTest : ExpectSpec({
 
     context("A year is not a leap year") {
@@ -18,12 +16,11 @@ class LeapYearTest : ExpectSpec({
         }
     }
 
-    context("A year is a leap year if it is divisible by 4") {
-        withData(1984,
-            2016,
-            2020,
-        ) { year ->
-            isLeapYear(year) shouldBe true
+    context("A year is a leap year") {
+        expect("if divisible by 4") {
+            checkAll(Arb.int().filter { it % 4 == 0 }) { a ->
+                isLeapYear(a) shouldBe true
+            }
         }
     }
 })

051f500ff3295908a5ffffb0c3e3462de0ba5662
lidonis <lidonis.calhau@gmail.com>
2021-07-22 22:32:07 +0200

    test: 2020 is leap year

--- src/test/kotlin/LeapYearTest.kt
+++ src/test/kotlin/LeapYearTest.kt
@@ -5,4 +5,8 @@ class LeapYearTest : ExpectSpec({
     expect("2021 is not a leap year") {
         isLeapYear(2021) shouldBe false
     }
+
+    expect("2020 is a leap year") {
+        isLeapYear(2020) shouldBe true
+    }
 })

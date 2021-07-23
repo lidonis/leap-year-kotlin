df6cc134c212b80b697be845d5bfbc7845c4c226
lidonis <lidonis.calhau@gmail.com>
2021-07-23 21:24:37 +0200

    refactor: rename test

--- src/test/kotlin/LeapYearTest.kt
+++ src/test/kotlin/LeapYearTest.kt
@@ -2,6 +2,7 @@ import io.kotest.core.spec.style.ExpectSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.property.Arb
 import io.kotest.property.arbitrary.filter
+import io.kotest.property.arbitrary.filterNot
 import io.kotest.property.arbitrary.int
 import io.kotest.property.checkAll
 
@@ -9,26 +10,26 @@ class LeapYearTest : ExpectSpec({
 
     context("A year is not a leap year") {
         expect("if not divisible by 4") {
-            checkAll(Arb.int().filter { !(it isDivisibleBy 4) }) { a ->
-                isLeapYear(a) shouldBe false
+            checkAll(Arb.int().filterNot { it isDivisibleBy 4 }) { year ->
+                isLeapYear(year) shouldBe false
             }
         }
         expect("if divisible by 100 but not by 400") {
-            checkAll(Arb.int().filter { (it isDivisibleBy 100) && !(it isDivisibleBy 400) }) { a ->
-                isLeapYear(a) shouldBe false
+            checkAll(Arb.int().filter { (it isDivisibleBy 100) && !(it isDivisibleBy 400) }) { year ->
+                isLeapYear(year) shouldBe false
             }
         }
     }
 
     context("A year is a leap year") {
         expect("if divisible by 4") {
-            checkAll(Arb.int().filter { it isDivisibleBy 4 }) { a ->
-                isLeapYear(a) shouldBe true
+            checkAll(Arb.int().filter { it isDivisibleBy 4 }) { year ->
+                isLeapYear(year) shouldBe true
             }
         }
         expect("if divisible by 400") {
-            checkAll(Arb.int().filter { it isDivisibleBy 400 }) { a ->
-                isLeapYear(a) shouldBe true
+            checkAll(Arb.int().filter { it isDivisibleBy 400 }) { year ->
+                isLeapYear(year) shouldBe true
             }
         }
     }

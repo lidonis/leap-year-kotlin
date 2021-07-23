d74957b6c0feb83f272bad8f8f4d6bb1e525e09b
lidonis <lidonis.calhau@gmail.com>
2021-07-23 21:26:03 +0200

    impl: final impl

--- src/main/kotlin/IsLeapYear.kt
+++ src/main/kotlin/IsLeapYear.kt
@@ -1,3 +1,9 @@
-fun isLeapYear(year: Int) = year isDivisibleBy 4
+fun isLeapYear(year: Int): Boolean {
+    return if (year isDivisibleBy 4) {
+        if (year isDivisibleBy 100) {
+            year isDivisibleBy 400
+        } else true
+    } else false
+}
 
 infix fun Int.isDivisibleBy(divider: Int) = this % divider == 0
